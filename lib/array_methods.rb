e8125162a6b0c706bb0d24e5ddb109a6f625e4ab
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 15:38:17 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
-def using_include(array, element)
 
+
+
+def using_include(array1 = ["Spring", "Summer", "Fall", "Winter"], element = Fall)
+  array1.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort(array1 = ["Spring", "Summer", "Fall", "Winter"])
+  array1.sort!
+  return array1
 end
 
-def using_reverse(array)
-
+def using_reverse(array1 = ["Spring", "Summer", "Fall", "Winter"])
+  array1.reverse
 end
 
-def using_first(array)
-
+def using_first(array1 = ["Spring", "Summer", "Fall", "Winter"])
+  return array1.first
 end
 
-def using_last(array)
-
+def using_last(array1 = ["Spring", "Summer", "Fall", "Winter"])
+  return array1.last
 end
 
-def using_size(array)
-
+def using_size(array1 = ["Spring", "Summer", "Fall", "Winter"])
+  return array1.size
 end
